068fb590194cc20d9c59e81261b2a16bb055c871
TatShar <tvku@yandex.ru>
2022-08-11 10:21:51 +0300

    task2 второе решение

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,16 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = number%100;
-int result1 = result%10;
-int result2 = (result - result1)/10;
+//Console.Write("Введите трехзначное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int result = number%100;
+//int result1 = result%10;
+//int result2 = (result - result1)/10;
 
-if (number>99 | number <1000)
-{
-    Console.Write(result2);
+//if (number>99 | number <1000)
+//{
+ //   Console.Write(result2);
+//}
+//if (number<=99 | number >=1000)
+//{
+//    Console.Write("uncorrect number");
+//}
+
+Console.Write("input three-digit number ");
+int number = Convert.ToInt32(Console.ReadLine());
+String numberT = Convert.ToString(number);
+
+if (numberT.Length <4 && numberT.Length >2){
+    Console.Write("secomd number is " + numberT[1]);
 }
-if (number<=99 | number >=1000)
-{
+else {
     Console.Write("uncorrect number");
 }
